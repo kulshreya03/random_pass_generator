5e63025be08d1c5c94dff7b36fd2dc43bc84ee5e
kulshreya03 <kulkarnishreya04@gmail.com>
2024-05-29 15:10:20 +0530

    Pass generator

---
+++ pass_generator.py
@@ -0,0 +1,49 @@
+import string
+import random
+import bcrypt
+import logging
+
+def generate_pass(len):
+
+    #character set of all elements of password
+    #all possible characters
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
+    digits = string.digits
+    special_char = string.punctuation
+
+    #combine all characters
+    all_chars = lower+upper+digits+special_char
+
+    #create pass ensuring at least one char from each
+    password = [random.choice(lower) + random.choice(upper) + random.choice(digits) + random.choice(special_char)]
+
+    #fill remaining chars with random
+    #k->no. of items
+    password += ''.join(random.choices(all_chars,k=len-4))
+
+    #shuffle for better randomness
+    random.shuffle(password)
+
+    #return as a string
+    return ''.join(password)
+
+
+
+
+file='hashed_pass.log'
+logging.basicConfig(filename=file,filemode='a', level=logging.INFO, format='%(levelname)s - %(message)s - %(asctime)s')
+
+len = int(input('Enter the length of pass>>'))
+if len<8:
+    print('Password should be atleast 8 characters long!!')
+    logging.error('Less than 8')
+
+else:
+    password = generate_pass(len)
+    print(f'Your randomly generated password is >>{password}')
+
+    #store hashed in log
+    pw = password
+    hashed = bcrypt.hashpw(pw.encode(), bcrypt.gensalt()).decode()
+    logging.info(f'Hashed Passwrd>>{hashed}')
